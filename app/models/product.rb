f240eeabc4ba40c933f199e53dda6f377becdac9
Glen Warren <glenwarren@zoho.com>
2019-10-22 16:48:24 +0100

    FEAT: added validations to all models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,20 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :filters
+  has_many :sub_categories, through: :filters
+  validates :name, presence: true
+  validates :description, presence: true
+  # validates :image, presence: true
+  validates :price, presence: true
+  validates :availability, presence: true
+  before_save :default_active
+  before_save :default_homepage
+
+  def default_active
+    self.active ||= true
+  end
+
+  def default_homepage
+    self.homepage ||= false
+  end
 end
--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,3 +1,15 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
+  has_many :filters, dependent: :destroy
+  validates :name, presence: true
+  before_save :default_active
+  before_save :default_homepage
+
+  def default_active
+    self.active ||= true
+  end
+
+  def default_homepage
+    self.homepage ||= false
+  end
 end
