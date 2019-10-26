59a3803a17fbb64fa8011e4a9e032051362284ab
Glen Warren <glenwarren@zoho.com>
2019-10-26 01:05:45 +0100

    FEAT: working on add to basket option

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,10 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(user: current_user, status: "Basket")
+    @order_item = OrderItem.new(order: @order, product: params[:id], quantity: 1)
+    @order.save
+    @order_item.save
   end
 
   def edit
@@ -20,3 +24,10 @@ class OrdersController < ApplicationController
   def destroy
   end
 end
+
+# - user clicks button (eventlistener)
+# - order = Order.where(user: current_user).find_by(status: "Basket") ? order : order = Order.create(user: current_user)
+# - check if user has an order with status "Basket"
+# - if not then create one
+# - Create a new order_item, quantity 1
+# -
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
-  validates :order, uniqueness: { scope: :product }
+  # validates :order, uniqueness: { scope: :product }
 end
