8b30b7b137114c04f6ccc2cbb8ad2aa905175e8a
Glen Warren <glenwarren@zoho.com>
2019-10-27 12:42:34 +0000

    FIX: sorted out some issues, and made it that a user has to be signed in to have a basket, for the time being

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :set_variables
 
   def set_variables
-    user_signed_in? ? @user = current_user : @user = session[:current_user_id]
+    # user_signed_in? ? @user = current_user : @user = session.id
+    # session[:current_user_id]
     # byebug
+    @user = current_user
     basket = Order.find_by(user: @user, status: "Basket" || "Address")
     @order_items = OrderItem.where(order: basket)
     @user_basket = Hash.new(0)
@@ -14,3 +16,7 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+# Create a guest_basket model
+# Tied to session.id
+# Once they log-in, transfer all the data to their Order basket?
